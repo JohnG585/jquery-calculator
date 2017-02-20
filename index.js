67d31dc14be5a9fa1101f863eee0b02e912b5dc6
JohnG585 <JGallagher585@Gmail.com>
2017-02-20 15:25:57 -0700

    commit3
    redid eval

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ $(document).ready(function() {
   const screen = $('#screen');
   let span = $('span');
   let equals = $('#equals');
+  // let noOps = $('span:not[className = "operator"]');
 
   $('#clear').click(function() {
     screen.text('0');
+    results.clear()
   });
 
-      $('div.buttons').click(('span:not([class="operator"])'),
-      function(event) {
+  $('div.buttons').click('span:not("#clear"):not("#equals")',
+      function() {
+        const newStr = screen.text() + $(event.target).text();
 
-        let newStr = screen.text() + $(event.target).text();
-        console.log(newStr)
-        screen.text(newStr)
+        // console.log(newStr)
+        screen.text(newStr);
       });
 
     equals.click(function() {
       try {
       let result = eval(screen.text().replace(/x/, '*').replace(/÷/, '/'));
+      result = screen.text()+" = "+result;
       screen.text(result);
     }
       catch(e) {
